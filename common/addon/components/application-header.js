d85e5f90a6569958be34a9d8ccec33a15e2027b6
ShuoC <shuo@pbjelli.com>
2015-10-26 09:45:51 -0700

    added more integration tests

---
+++ common/addon/components/application-header.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import layout from '../templates/components/application-header';
+
+export default Ember.Component.extend({
+  	layout: layout,
+  	controller: null,
+	isLoading: true,
+	classNames: ['application-header'],
+	partnerLogoUrl: Ember.computed('', function(){
+		var user = this.container.lookup('session:user');
+		console.log('application-header', user);
+		return user.get('partnerLogoUrl');
+	}),
+	currentUser: Ember.computed('', function(){
+		this.set('isLoading', false);
+		return this.container.lookup('session:user');
+	}),
+	actions: {
+		logout: function(){
+			console.log('sendAction logout');
+			this.sendAction('action');
+		}
+	}
+});
